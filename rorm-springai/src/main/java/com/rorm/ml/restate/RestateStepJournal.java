f769c0041826aa8b199992bee9a0105e54476dee
Evemose <rotan.olexandr@gmail.com>
2026-03-17 03:38:17 +0200

    Enhance RestateStepJournal to utilize ObjectContext for state management; implement caching mechanism for step results; add crossInvocationReplay test for verifying state replay functionality.

--- rorm-springai/src/main/java/com/rorm/ml/restate/RestateStepJournal.java
+++ rorm-springai/src/main/java/com/rorm/ml/restate/RestateStepJournal.java
@@ -1,7 +1,8 @@
 package com.rorm.ml.restate;
 
 import com.rorm.durable.StepJournal;
-import dev.restate.sdk.Context;
+import dev.restate.sdk.ObjectContext;
+import dev.restate.sdk.common.StateKey;
 import lombok.RequiredArgsConstructor;
 
 import java.util.function.Supplier;
@@ -9,10 +10,17 @@ import java.util.function.Supplier;
 @RequiredArgsConstructor
 public class RestateStepJournal implements StepJournal {
 
-    private final Context ctx;
+    private final ObjectContext ctx;
 
     @Override
     public <T> T run(String stepName, Class<T> resultType, Supplier<T> action) {
-        return ctx.run(stepName, resultType, action::get);
+        var stateKey = StateKey.of(stepName, resultType);
+        var cached = ctx.get(stateKey);
+        if (cached.isPresent()) {
+            return cached.get();
+        }
+        var result = ctx.run(stepName, resultType, action::get);
+        ctx.set(stateKey, result);
+        return result;
     }
 }
